b156ec2606e93e4b403491e5bbbcbe6b44938138
Anirban <anirban9973@proton.me>
2025-08-09 16:53:27 +0800

    commit from laptop

--- init.el
+++ init.el
@@ -280,7 +280,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(julia-snail multi-vterm vterm which-key julia-repl julia-mode magit page-break-lines org-ref biblio doom-themes counsel-projectile yasnippet-snippets writegood-mode projectile markdown-mode eglot-inactive-regions counsel company auctex)))
+   '(org-ref biblio vterm magit ivy yasnippet which-key julia-mode julia-snail company-auctex cuda-mode page-break-lines doom-themes counsel-projectile yasnippet-snippets writegood-mode projectile markdown-mode counsel company auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
